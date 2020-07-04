9d99d5d76323d7b79a9130ba51a02961e71ae911
skamelone <skamelone@gmail.com>
2020-07-04 12:19:53 -0500

    [+] Install pynvim

--- install.sh
+++ install.sh
@@ -1,28 +1,73 @@
 #!/bin/bash
 
-set -o nounset # error when referencing undefined variable
+set -o nounset # error when referencing undefined varinstallextrapackagenvimiable
 set -o errexit # exit when error fails
 
 installhomebrew() { \
+  echo "Installing Homebrew"
   /bin/bash -c "$(curl -fssl https://raw.githubusercontent.com/homebrew/install/master/install.sh)"
-  echo "Homebrew installed"
 }
 
 installgit() { \
+  echo "Installing git..."
   brew install git
 }
 
 installpython() { \
+  echo "Installing python3..."
   brew install python3
 }
 
-echo 'Installing Bootstrap'
+installpynvim() { \
+  echo "Installing pynvim..."
+  pip3 install pynvim
+}
 
-# Install Homebrew
-#installhomebrew
+installnvim() { \
+  echo "Installing nvim"
+  brew install neovim
+}
 
-# Install Python
-which pip3 > /dev/null && echo "pip installed, moving on..." || installpython
+installextrapackagenvim() { \
+  echo "Installing nvim extra package"
+  brew install ripgrep fzf ranger
+}
+
+alreadyinstallmessage() { \
+   printf "$1 already installed\t✅\n" | expand -t 60
+}
+
+moveoldnvim() { \
+  echo "Backing up your config to nvim.old"
+  mv $HOME/.config/nvim $HOME/.config/nvim.old
+}
+
+cloneconfig() { \
+  [ -d "$HOME/.config/nvim" ] && moveoldnvim 
+  echo "Cloning Neovim configuration"
+  git clone https://github.com/skamelone/nvim.git ~/.config/nvim
+}
+
+printf '\nInstalling Bootstrap\n'
+printf '**********************\n\n'
 
 # Install git
-# which brew > /dev/null && "Git installed" || installgit
+which git > /dev/null && alreadyinstallmessage "Git" || installgit
+
+# Install Homebrew
+which brew > /dev/null && alreadyinstallmessage "Homebrew" || installhomebrew
+
+# Install Python
+which pip3 > /dev/null && alreadyinstallmessage "Pip" || installpython
+
+# Install pynvim
+pip3 list | grep pynvim > /dev/null && alreadyinstallmessage "pynvim" || installpynvim
+
+# Install Neovim
+which nvim > /dev/null && alreadyinstallmessage "nvim" || installnvim
+
+# Install Nvim extrapackage
+which fzf > /dev/null && alreadyinstallmessage "nvim extras" || installextrapackagenvim
+
+# Clone nvim config
+cloneconfig
