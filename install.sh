18b1f4fceb2106448f15be411f65a61612b22ab7
skamelone <skamelone@gmail.com>
2020-07-04 11:56:23 -0500

    [*] Update homebrew install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -o nounset # error when referencing undefined variable
 set -o errexit # exit when error fails
 
-installhomebrew() { \
+installHomeBrew() { \
   /bin/bash -c "$(curl -fssl https://raw.githubusercontent.com/homebrew/install/master/install.sh)"
   echo "Homebrew installed"
 }
