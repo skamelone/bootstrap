642d2819aadea31257160e2a9424529804d66b31
skamelone <skamelone@gmail.com>
2020-07-04 22:28:32 -0500

    [+] Xcode install

--- dev.sh
+++ dev.sh
@@ -9,11 +9,19 @@ GREEN=$(tput setaf 2)
 RED=$(tput setaf 1)
 BOLD=$(tput bold)
 
+installxcode() { \
+  emsg "Xcode installing1..."
+  sudo gem install xcode-install
+  xcversion install $XCODE_DESIRED_VERSION 
+  mv ~/Downloads/Xcode* /Applications
+}
+
 installrbenv() { \
   brew install rbenv
   LINE='eval "$(rbenv init -)"'
 契grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 }
+
 #########################################################################
 ################################ UTIL ###################################
 #########################################################################
@@ -36,24 +44,25 @@ etitle() { \
 #########################################################################
 
 etitle "Installing Dev environment"
+[ -d /Applications/Xcode*.app ] && alreadyinstallmessage "Xcode" || installxcode 
 
-# Install Swiftlint
-which swiftlint > /dev/null && alreadyinstallmessage "Swiftlint" || brew install swiftlint
+# # Install Swiftlint
+# which swiftlint > /dev/null && alreadyinstallmessage "Swiftlint" || brew install swiftlint
 
-# Install Adr-List
-which adr-list > /dev/null && alreadyinstallmessage "ADR Tools" || brew install adr-tools
+# # Install Adr-List
+# which adr-list > /dev/null && alreadyinstallmessage "ADR Tools" || brew install adr-tools
 
-# Install SwiftGen
-which swiftgen > /dev/null && alreadyinstallmessage "SwiftGen" || brew install swiftgen
+# # Install SwiftGen
+# which swiftgen > /dev/null && alreadyinstallmessage "SwiftGen" || brew install swiftgen
 
-# Install Carthage
-which carthage > /dev/null && alreadyinstallmessage "Carthage" || brew install carthage
+# # Install Carthage
+# which carthage > /dev/null && alreadyinstallmessage "Carthage" || brew install carthage
 
-# Install Fastlane
-which fastlane > /dev/null && alreadyinstallmessage "Fastlane" || brew cask install fastlane
+# # Install Fastlane
+# which fastlane > /dev/null && alreadyinstallmessage "Fastlane" || brew cask install fastlane
 
-# Install Ruby-build
-which ruby-build > /dev/null && alreadyinstallmessage "Ruby-build" || brew install ruby-build
+# # Install Ruby-build
+# which ruby-build > /dev/null && alreadyinstallmessage "Ruby-build" || brew install ruby-build
 
-# Install Rbenv
-which rbenv > /dev/null && alreadyinstallmessage "Rbenv" || installrbenv
+# # Install Rbenv
+# which rbenv > /dev/null && alreadyinstallmessage "Rbenv" || installrbenv
