579508a9541c07a3b68cab83d9115fe96af10daa
skamelone <skamelone@gmail.com>
2020-07-14 21:26:06 -0500

    [+] Add Kitty installation

---
+++ experimental.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -o nounset # error when referencing undefined varinstallextrapackagenvimiable
+set -o errexit # exit when error fails
+
+# Various log colors
+RESET=$(tput sgr0)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+BOLD=$(tput bold)
+
+installkitty() { \
+  emsg "Kitty install"
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  wget https://raw.githubusercontent.com/skamelone/bootstrap/master/config/kitty/kitty.zip
+  unzip kitty.zip
+  mv kitty $HOME/.config/
+  rm kitty.zip
+}
+
+
+#########################################################################
+################################ UTIL ###################################
+#########################################################################
+
+alreadyinstallmessage() { \
+   printf "✅ $1 already installed\n" | expand -t 60
+}
+
+emsg() { \
+  printf "\n\n⚙️  %s%s%s%s\n\n" $BOLD $GREEN "$1" $RESET
+}
+
+etitle() { \
+  printf "\n%s%s%s%s\n" $BOLD $RED "$1" $RESET
+  printf "%s%s%s%s\n" $BOLD $RED "**********************" $RESET
+}
+
+#########################################################################
+################################ DEVS ###################################
+#########################################################################
+
+etitle "Installing Kitty"
+
+# Install Swiftlint
+which kitty > /dev/null && alreadyinstallmessage "Kitty" || installkitty
+
